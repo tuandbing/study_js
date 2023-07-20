c2913c6b9366d7ef571a1c1360a51b104b1f43a0
tuandbing <1873656423@qq.com>
2023-07-20 18:28:16 +0800

    修改

---
+++ day2/index.js
@@ -0,0 +1,10 @@
+// alert(18 == '18'); // true 
+// alert(18 === '18'); // false
+// var num = 17;
+// var num2 = 18;
+// // alert(num > num2 && num > 15);
+// alert(num && num2);
+
+var num1 = prompt('请输入第一个数字');
+var num2 = prompt('请输入第二个数字');
+alert(num1 > num2 ? num1 : num2);
